31ec85df364fbfd0cd4ebac9afed64e21b04686d
Kaushik <kmaz285@gmail.com>
2026-08-12 14:12:16 -0400

    Fix legal-links domain typo: nestexpensetraker -> nestexpensetracker

--- lib/legalLinks.ts
+++ lib/legalLinks.ts
@@ -1,2 +1,2 @@
-export const PRIVACY_POLICY_URL = 'https://nestexpensetraker.legal.craftloop.ca/privacy';
-export const TERMS_OF_SERVICE_URL = 'https://nestexpensetraker.legal.craftloop.ca/terms';
+export const PRIVACY_POLICY_URL = 'https://nestexpensetracker.legal.craftloop.ca/privacy';
+export const TERMS_OF_SERVICE_URL = 'https://nestexpensetracker.legal.craftloop.ca/terms';
